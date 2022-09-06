144b0702b16cc0433f5e2a98a1c37e9849572922
Mrxtyyp <1126084777@qq.com>
2022-09-06 09:51:07 +0800

    feat: typescript-absolute 第二种解法

--- typescript/src/type-challenges/medium/00529-medium-absolute/index.ts
+++ typescript/src/type-challenges/medium/00529-medium-absolute/index.ts
@@ -1,9 +1,11 @@
-// type Absolute<T extends number | string> = 
-//     `${T}` extends `${infer First}${infer Other}` ? 
-//         First extends number ? 
-//             `${First}${Absolute<Other>}` : 
-//                 `${Absolute<Other>}`
-//     : ''
-
+// 解法一
 type Absolute<T extends number | string | bigint> = 
-`${T}` extends `-${infer S}` ? S : `${T}`
+    `${T}` extends `${infer First}${infer Other}` ? 
+        First extends `${number}` ? 
+            `${First}${Absolute<Other>}` : 
+                `${Absolute<Other>}`
+    : ''
+
+// 解法二
+// type Absolute<T extends number | string | bigint> = 
+// `${T}` extends `-${infer S}` ? S : `${T}`
