69e265d50510db816d6013bb57435af4e141c752
Mrxtyyp <1126084777@qq.com>
2022-09-16 12:51:07 +0800

    feat: typescript-starts-with

--- typescript/src/type-challenges/medium/02688-medium-startswith/index.ts
+++ typescript/src/type-challenges/medium/02688-medium-startswith/index.ts
@@ -1,1 +1,3 @@
-type StartsWith<T extends string, U extends string> = T extends `${U}${infer _}` ? true : false
+type StartsWith<T extends string, U extends string> = 
+    [U] extends [''] ? true : 
+        T extends `${U}${infer _}` ? true : false
