215f2d4374a0f4c0941abee3a79e9cb0cb047de7
Mrxtyyp <1126084777@qq.com>
2022-09-29 09:51:00 +0800

    feat: test

--- typescript/src/test.ts
+++ typescript/src/test.ts
@@ -9,3 +9,10 @@ type Without<T, U> =
 type Res = Without<[1, 2], 1> // expected to be [2]
 type Res1 = Without<[1, 2, 4, 1, 5], [1, 2]> // expected to be [4, 5]
 type Res2 = Without<[2, 3, 2, 3, 2, 3, 2, 3], [2, 3]> // expected to be []
+
+
+type Case = number | number[]
+function hello(n: Case) {
+    console.log(n);
+}
+hello(123)
